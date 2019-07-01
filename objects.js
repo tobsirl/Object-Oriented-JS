5985ef666bc5743e50090c0ad83d38336a1d8a96
Paul Tobin <tobster111@gmail.com>
2019-07-01 19:44:04 +0100

    Example of an object literal

---
+++ objects.js
@@ -0,0 +1,13 @@
+const userOne = {
+  email: 'ryu@ninjas.com',
+  name: 'Ryu',
+  login() {
+    console.log(this.email, 'has logged in');
+  },
+  logout() {
+    console.log(this.email, 'has logged out');
+  }
+};
+
+userOne.login();
+userOne.logout();
